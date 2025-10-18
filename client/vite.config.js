808ffb257cf23bb49168b6e4cf05630225b0d447
Marlette Dakiwas <mjessdakiwas@gmail.com>
2025-10-18 00:53:02 -0400

    Debugging serving frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: '0.0.0.0',
-        port: process.env.PORT || 5173,
         cors: true,
         proxy: {
             '/api': {
