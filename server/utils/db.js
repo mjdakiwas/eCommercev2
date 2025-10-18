9ff7b59b7c49b9f11561db06cacad6d660aa9f20
Marlette Dakiwas <mjessdakiwas@gmail.com>
2025-10-17 23:07:35 -0400

    Debugged database connection

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql2';
 import 'dotenv/config';
 
-const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL).promise();
+const pool = mysql.createPool(process.env.MYSQL_URL).promise();
 
 export default pool;
