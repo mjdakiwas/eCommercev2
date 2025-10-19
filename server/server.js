7184286d0612bc778986b8cc8dc3395e4b4ba642
Marlette Dakiwas <mjessdakiwas@gmail.com>
2025-10-19 18:25:02 -0400

    Debugging serving frontend

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import pool from './utils/db.js';
-import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,10 +12,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, 'dist')));
-
 app.get('/api/products', async (req, res) => {
     try {
         const [rows] = await pool.query(`SELECT * FROM products`);
@@ -25,10 +20,6 @@ app.get('/api/products', async (req, res) => {
         console.log(err);
         res.json({ error: 'Error fetching from database' });
     }
-});
-
-app.get('/{*splat}', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
